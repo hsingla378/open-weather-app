f05b385d3312b975b0a9141a31368a6e90833269
hsingla378 <hsingla378@gmail.com>
2022-07-04 13:09:03 +0530

    added city input

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 const { json } = require("body-parser")
 const express = require("express")
+const bodyParser = require("body-parser")
+const fs = require("fs")
 
 const app = express()
 const https = require("https")
+const { fstat } = require("fs")
+const { send } = require("process")
 
-app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Sirsa&appid=54033f0864dc6ac6da9c0e8af059c847&units=metric"
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res, next){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", (req,res) =>{
+
+    const query = req.body.cityName;
+    const apiKey = "54033f0864dc6ac6da9c0e8af059c847";
+    const unit = "metric"
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +  "&units=" + unit
 
     https.get(url, function(response){
-        console.log(response.statusCode)
+        console.log("Status code: " + response.statusCode)
 
         response.on("data", function(data){
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
             const description = weatherData.weather[0].description
-            // console.log(description)
-            res.send(`current weather in sirsa: ${description}`)
+            const icon = weatherData.weather[0].icon;
+            const imageUrl =  "http://openweathermap.org/img/wn/" + icon  + "@2x.png"
+            res.write(`<h1>current weather in ${query}: ${description} </h1>`)
+            res.write("<img src=" + imageUrl + ">")
+            res.send()
         })
     })
 })
+
+
 
 
 
